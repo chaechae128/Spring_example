9d9a993975449b9bf303ff1ca89e6b742634a423
test <test@test.com>
2023-12-19 18:43:08 +0900

    lesson03 ex02완료 ex03진행중

---
+++ src/main/java/com/example/lesson03/Lesson03Ex02RestController.java
@@ -0,0 +1,35 @@
+package com.example.lesson03;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.lesson03.bo.ReviewBO;
+import com.example.lesson03.domain.Review;
+
+@RestController
+public class Lesson03Ex02RestController {
+	
+	@Autowired
+	private ReviewBO reviewBO;
+	//url: http://localhost:8008/lesson03/ex02/1
+	@RequestMapping("/lesson03/ex02/1")
+	public String ex02_1() {
+		Review review = new Review();
+		review.setStoreId(7);
+		review.setMenu("삼겹혼밥세트");
+		review.setUserName("권죵");
+		review.setPoint(4.5);
+		review.setReview("혼자 먹기 딱 적당하네요~");
+		int rowCount = reviewBO.addReview(review);  //insert 후 성공한 row 수를 리턴 받는다.
+		return "성공한 행의 개수:" + rowCount; //String + int => string
+	}
+	
+	
+	//url: http://localhost:8008/lesson03/ex02/2
+	@RequestMapping("/lesson03/ex02/2")
+	public String ex02_2() {
+		int rowCount = reviewBO.addReviewAsField(4, "콤비네이션R", "바다", 5.0, "역시 맛있다!");
+		return "성공한 행의 개수:" + rowCount;
+	}
+}
--- src/main/java/com/example/lesson03/bo/ReviewBO.java
+++ src/main/java/com/example/lesson03/bo/ReviewBO.java
@@ -17,4 +17,20 @@ public class ReviewBO {
 	public Review getReviewById(int id) { // 만약 request로 id 값이 null 로 오면 BO쪽에서 null인걸 받을 수 없어서 에러
 		return  reviewMapper.selectReviewById(id);
 	}
+	//input: Review		output: 성공한 행의 개수(int)
+	public int addReview(Review review) { 
+		
+		return reviewMapper.insertReview(review);
+	}
+	
+	//addReviewAsField(4, "콤비네이션R", "바다", 5.0, "역시 맛있다!");  //순서도 맞아야됨
+	public int addReviewAsField(int storeId, String menu, String userName, Double point, String review) {
+		//자동완성을 믿지 말고 순서를 잘 봐여됨
+		return reviewMapper.insertReviewAsField(storeId, menu, userName, point, review);
+	}
+	//input :id, review  output: 성공한 행의 개수
+	public int updateReviewById(int id, String review) {
+		return reviewMapper.updateReviewById(id, review);
+	}
+	
 }
