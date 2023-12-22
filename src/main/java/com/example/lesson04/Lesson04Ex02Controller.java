f2fddb8780b9b870d561da2ccb05d83ca6502a1b
kimchaeyeon <kelly021128@naver.com>
2023-12-22 13:39:19 +0900

    lesson04 ex02 완료

--- src/main/java/com/example/lesson04/Lesson04Ex02Controller.java
+++ src/main/java/com/example/lesson04/Lesson04Ex02Controller.java
@@ -2,6 +2,7 @@ package com.example.lesson04;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,17 +26,20 @@ public class Lesson04Ex02Controller {
 	//방금 가입 된 학생 화면 & DB insert
 	@PostMapping("/add-student")
 	public String addStudent(
-			@ModelAttribute Student student//@ModelAttribute 생략 가능하지만 붙이는 게 좋음
+			@ModelAttribute Student student,//@ModelAttribute 생략 가능하지만 붙이는 게 좋음
 			// 태그의 name 속성 파라미터와 이름이 같은 필드에 매핑 
 			//실무에서는 @RequestParam으로 함
-			){
+			Model model){
 		
 		//DB insert
 		studentBO.addStudent(student); //아까 id값 받도록 했는데 그 id는 student에 들어가있음
 		//DB에서 방금 가입된 사용자 select
 		int id = student.getId();
-		studentBO.getStudentById(id);
+		student = studentBO.getStudentById(id);
+		
 		//그 가져온 데이터를 model에 담는다(jsp에서 사용하도록)
+		model.addAttribute("student", student);
+		model.addAttribute("title", "방금 가입된 사용자 정보");
 		
 		//화면뿌리기 => table로
 		return "lesson04/afterAddStudent";
